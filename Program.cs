0116a46cd9a72b1ccd7f9ba3ef73a5ca0967ac39
joshu <joshu@REEDPC>
2024-06-03 13:12:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+
+            string number = Console.ReadLine();
+            int sum = 0; 
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += number[i] - '0';
+            }
+
+            Console.WriteLine("The sum of the digits is: " + sum);
+        }
+
+        
+    }
+}
